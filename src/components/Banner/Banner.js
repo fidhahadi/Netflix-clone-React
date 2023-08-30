e967d5c30453dfaa5271099db5df167b1e41555d
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-08-30 06:53:21 +0400

    start

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner-buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='description'>Lorem</h1>
+        </div> 
+    </div>
+  )
+}
+
+export default Banner
