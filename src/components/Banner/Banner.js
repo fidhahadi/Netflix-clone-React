fa20942f65ca30e9fd429779e0a1b280a545408f
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-08-30 10:36:44 +0400

    screen size

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -11,7 +11,7 @@ function Banner() {
     useEffect(() => {
         axios.get(`trending/all/week?api_key=${ API_KEY }&language=en-US`).then((response)=>{
            console.log(response.data?.results[2]); 
-           setMovie(response.data?.results[5]);
+           setMovie(response.data.results.sort(function (a, b) { return 0.5 - Math.random() })[0]);
         })
     }, [])
     
